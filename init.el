86ca78a98acae60453cbaad9d938c6f600927e00
Bruce Aberdeen <baberdeen@cbnl.com>
2014-01-03 20:03:32 +0000

    fix up xterm mouse mode

--- init.el
+++ init.el
@@ -83,5 +83,44 @@
 (require 'python-mode)
 
 
+;; xterm mouse stuff
 
+(defun my-terminal-config (&optional frame)
+  "Establish settings for the current terminal."
+  (if (not frame) ;; The initial call.
+      (xterm-mouse-mode 1)
+    ;; Otherwise called via after-make-frame-functions.
+    (if xterm-mouse-mode
+        ;; Re-initialise the mode in case of a new terminal.
+        (xterm-mouse-mode 1))))
+;; Evaluate both now (for non-daemon emacs) and upon frame creation
+;; (for new terminals via emacsclient).
+(my-terminal-config)
+(add-hook 'after-make-frame-functions 'my-terminal-config)
+
+;;;                                                                                                                                                                      
+;; ===== Scrolling fix for advanced mouses =============================                                                                                                  
+;; Cos mouse wheel mode has mysteriously vanished on suse 12.2                                                                                                            
+;;;;                                                                                                                                                                      
+                                                                                                                                                                          
+(defun up-slightly () (interactive) (scroll-up 5))                                                                                                                        
+(defun down-slightly () (interactive) (scroll-down 5))                                                                                                                    
+(global-set-key [mouse-4] 'down-slightly)                                                                                                                                 
+(global-set-key [mouse-5] 'up-slightly)                                                                                                                                   
+; Scroll up five lines with META held                                                                                                                                     
+(global-set-key [M-mouse-4] 'down-slightly)                                                                                                                               
+(global-set-key [M-mouse-5] 'up-slightly)                                                                                                                                 
+; Scroll up one line with SHIFT held                                                                                                                                      
+(defun up-one () (interactive) (scroll-up 1))                                                                                                                             
+(defun down-one () (interactive) (scroll-down 1))                                                                                                                         
+(global-set-key [S-mouse-4] 'down-one)                                                                                                                                    
+(global-set-key [S-mouse-5] 'up-one)                                                                                                                                      
+; Scroll up one page with CTRL held                                                                                                                                       
+;;(defun up-a-lot () (interactive) (scroll-up))                                                                                                                           
+;;(defun down-a-lot () (interactive) (scroll-down))                                                                                                                       
+;;(global-set-key [C-mouse-4] 'down-a-lot)                                                                                                                                
+;;(global-set-key [C-mouse-5] 'up-a-lot)                                                                                                                                  
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-decrease)                                                                                                                 
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-increase)                                                                                                                 
+;;;;                                                                          
 
